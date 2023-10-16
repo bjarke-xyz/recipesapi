112d4ec3a1b10c5edf726f7fd3217c7dbf895a15
bjarkt <688414+bjarkt@users.noreply.github.com>
2023-10-16 19:17:27 +0200

    refresh product feeds on startup

--- RecipesAPI.API/Infrastructure/HangfireRecurringJobs.cs
+++ RecipesAPI.API/Infrastructure/HangfireRecurringJobs.cs
@@ -17,6 +17,10 @@ public class HangfireRecurringJobs : BackgroundService
         Hangfire.RecurringJob.AddOrUpdate<RecipeService>(nameof(RecipeService.GetRecipe), s => s.GetRecipes(CancellationToken.None, null), "0 * * * *");
         Hangfire.RecurringJob.AddOrUpdate<AdtractionService>("adtraction_" + nameof(AdtractionService.RefreshProductFeeds), s => s.RefreshProductFeeds("DK", config.GetValue<int>("AdtractionChannelId")), "0 * * * *");
         Hangfire.RecurringJob.AddOrUpdate<PartnerAdsService>("partnerads_" + nameof(PartnerAdsService.RefreshProductFeeds), s => s.RefreshProductFeeds(), "0 * * * *");
+
+        Hangfire.RecurringJob.TriggerJob("adtraction_" + nameof(AdtractionService.RefreshProductFeeds));
+        Hangfire.RecurringJob.TriggerJob("partnerads_" + nameof(PartnerAdsService.RefreshProductFeeds));
+
         return Task.CompletedTask;
     }
 }
