c5fdb06078f936cfea8f59457b36db496d5a87a9
bjarkt <688414+bjarkt@users.noreply.github.com>
2023-10-25 21:43:59 +0200

    add ItemInfo to AffiliateItem

--- RecipesAPI.API/Features/Admin/Common/AffiliateItemReference.cs
+++ RecipesAPI.API/Features/Admin/Common/AffiliateItemReference.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using RecipesAPI.API.Features.Admin.Common.Adtraction;
 
 namespace RecipesAPI.API.Features.Admin.Common;
@@ -9,15 +10,56 @@ public class AffiliateItem
     {
         Provider = AffiliateProvider.Adtraction;
         Adtraction = adtractionFeedProduct;
+        if (!string.IsNullOrEmpty(adtractionFeedProduct.Brand) && !string.IsNullOrEmpty(adtractionFeedProduct.TrackingUrl))
+        {
+            ItemInfo = new AffiliateItemInfo
+            {
+                Title = adtractionFeedProduct.Brand,
+                Url = adtractionFeedProduct.TrackingUrl,
+                ImageUrl = adtractionFeedProduct.ImageUrl,
+                Description = adtractionFeedProduct.Description,
+                Category = adtractionFeedProduct.Category,
+                NewPrice = adtractionFeedProduct.Price,
+                OldPrice = adtractionFeedProduct.OriginalPrice,
+                InStock = adtractionFeedProduct.InStock,
+            };
+        }
     }
     public AffiliateItem(PartnerAdsFeedProduct partnerAdsFeedProduct)
     {
         Provider = AffiliateProvider.PartnerAds;
         PartnerAds = partnerAdsFeedProduct;
+        if (!string.IsNullOrEmpty(partnerAdsFeedProduct.Brand) && !string.IsNullOrEmpty(partnerAdsFeedProduct.ProductUrl))
+        {
+            ItemInfo = new AffiliateItemInfo
+            {
+                Title = partnerAdsFeedProduct.Brand,
+                Url = partnerAdsFeedProduct.ProductUrl,
+                ImageUrl = partnerAdsFeedProduct.ImageUrl,
+                Description = partnerAdsFeedProduct.Description,
+                Category = partnerAdsFeedProduct.CategoryName,
+                NewPrice = partnerAdsFeedProduct.NewPrice,
+                OldPrice = partnerAdsFeedProduct.OldPrice,
+                InStock = partnerAdsFeedProduct.InStock,
+            };
+        }
     }
     public AffiliateProvider Provider { get; set; }
     public AdtractionFeedProduct? Adtraction { get; set; }
     public PartnerAdsFeedProduct? PartnerAds { get; set; }
+    public AffiliateItemInfo? ItemInfo { get; set; } = new();
+}
+
+public class AffiliateItemInfo
+{
+    public string Title { get; set; } = "";
+    public string Url { get; set; } = "";
+    public string? ImageUrl { get; set; }
+    public string? Description { get; set; }
+    public string? Category { get; set; }
+    public decimal? NewPrice { get; set; }
+    public decimal? OldPrice { get; set; }
+    public bool? InStock { get; set; }
 }
 
 public class AffiliateItemReference
