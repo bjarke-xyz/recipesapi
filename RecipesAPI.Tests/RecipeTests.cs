9a51d8cda0e95db03dd6e87392020625b840b97c
bjarkt <688414+bjarkt@users.noreply.github.com>
2024-03-24 10:31:10 +0100

    disable testcontainer test

--- RecipesAPI.Tests/RecipeTests.cs
+++ RecipesAPI.Tests/RecipeTests.cs
@@ -12,23 +12,23 @@ public class RecipeTests
   [TestCase]
   public async Task TestTest()
   {
-    var container = new ContainerBuilder()
-      // remember to build Dockerfile.firebase using docker compose
-      .WithImage("bjarkt/firebase_emulator")
-      .WithPortBinding(4000, true)
-      .WithPortBinding(8080, true)
-      .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r.ForPort(4000)))
-      .Build();
+    // var container = new ContainerBuilder()
+    //   // remember to build Dockerfile.firebase using docker compose
+    //   .WithImage("bjarkt/firebase_emulator")
+    //   .WithPortBinding(4000, true)
+    //   .WithPortBinding(8080, true)
+    //   .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r.ForPort(4000)))
+    //   .Build();
 
-    await container.StartAsync()
-      .ConfigureAwait(false);
+    // await container.StartAsync()
+    //   .ConfigureAwait(false);
 
-    var httpClient = new HttpClient();
+    // var httpClient = new HttpClient();
 
-    var requestUri = new UriBuilder(Uri.UriSchemeHttp, container.Hostname, container.GetMappedPublicPort(4000), "/").Uri;
+    // var requestUri = new UriBuilder(Uri.UriSchemeHttp, container.Hostname, container.GetMappedPublicPort(4000), "/").Uri;
 
-    var resp = await httpClient.GetStringAsync(requestUri)
-      .ConfigureAwait(false);
+    // var resp = await httpClient.GetStringAsync(requestUri)
+    //   .ConfigureAwait(false);
 
     // Environment.SetEnvironmentVariable("FIRESTORE_EMULATOR_HOST", $"{container.Hostname}:${container.GetMappedPublicPort(8080)}");
     // var db = FirestoreDb.Create("demo");
