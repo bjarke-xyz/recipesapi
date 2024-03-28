b5837050de1a7f30784bd904f5452b58074a0db4
bjarkt <688414+bjarkt@users.noreply.github.com>
2024-03-28 14:00:06 +0100

    remove tf

--- terraform/compartment.tf
+++
@@ -1,7 +0,0 @@
-
-resource "oci_identity_compartment" "tf-compartment" {
-  # Required
-  compartment_id = var.tenancy_ocid
-  description    = "Compartment for recipesapi resources."
-  name           = "recipesapi"
-}
